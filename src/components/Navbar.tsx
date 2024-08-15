1618c39f19b52af6f43100ea3ee02700bad982cc
raj shringi <raj24shringi@gmail.com>
2024-08-15 18:06:52 +0530

    remove navbar shadow

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export default function Navbar() {
     <div className="">
       {/* <div className="absolute inset-0 -z-10 h-full w-full bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"></div> */}
       <div className="relative flex items-center max-w-[600px] mx-auto py-4">
-        <nav className="h-full w-full isolate rounded-xl bg-white/90 shadow-lg ring-1 ring-black/5 flex items-center justify-between p-4">
+        <nav className="h-full w-full isolate rounded-xl bg-white/90  ring-1 ring-black/5 flex items-center justify-between p-4">
           <Link href="/">
             <h2 className="text-xl font-medium uppercase tracking-widest">
               Flash

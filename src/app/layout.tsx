89525b6186f5b0fe8fa08779ec442fb4f21e6f20
raj shringi <raj24shringi@gmail.com>
2024-08-18 22:16:46 +0530

    add Toaster in layout to show Toaster

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
               @ All rights resevered
             </footer>
           </main>
+          <Toaster />
         </body>
       </html>
     </ClerkProvider>

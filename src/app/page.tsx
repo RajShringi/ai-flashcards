6e26c7757ef1eaadde9b271313a694d41359fb2d
raj shringi <raj24shringi@gmail.com>
2024-08-18 22:17:04 +0530

    add Subcription component in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,7 @@
+import Subscription from "@/components/Subscription";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  const subscriptions = [
-    {
-      id: 1,
-      name: "Basic Plan",
-      price: "$5/month",
-      features: ["Access to basic flashcards", "Limited AI assistance"],
-    },
-    {
-      id: 2,
-      name: "Pro Plan",
-      price: "$10/month",
-      features: [
-        "Access to all flashcards",
-        "Advanced AI assistance",
-        "Offline access",
-      ],
-    },
-    {
-      id: 3,
-      name: "Premium Plan",
-      price: "$20/month",
-      features: [
-        "All features of Pro",
-        "Personalized AI tutoring",
-        "Priority support",
-      ],
-    },
-  ];
-
   return (
     <div className="flex flex-col gap-8">
       {/* hero section */}
@@ -79,44 +51,7 @@ export default function Home() {
       </section>
 
       {/* subscription section */}
-      <section className="px-32 flex flex-col gap-6">
-        <h2 className="text-3xl font-semibold text-center">Select Your Plan</h2>
-        <div className="flex items-center justify-between gap-8">
-          {subscriptions.map((plan) => (
-            <div
-              key={plan.id}
-              className="self-stretch flex-1 flex flex-col items-center justify-between gap-4 rounded-lg border border-gray-300 px-4 py-8 bg-white"
-            >
-              <h3 className="text-2xl font-semibold">{plan.name}</h3>
-              <p className="text-3xl md:text-4xl font-bold">{plan.price}</p>
-              <ul className="features">
-                {plan.features.map((feature, index) => (
-                  <li className="text-gray-500 list-disc" key={index}>
-                    {feature}
-                  </li>
-                ))}
-              </ul>
-              {plan.name === "Basic Plan" ? (
-                <Button variant="link">Choose Plan</Button>
-              ) : (
-                ""
-              )}
-
-              {plan.name === "Pro Plan" ? (
-                <Button variant="secondary">Choose Plan</Button>
-              ) : (
-                ""
-              )}
-
-              {plan.name === "Premium Plan" ? (
-                <Button variant="default">Choose Plan</Button>
-              ) : (
-                ""
-              )}
-            </div>
-          ))}
-        </div>
-      </section>
+      <Subscription />
     </div>
   );
 }
